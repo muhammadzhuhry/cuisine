48d96d5a690ed7a1c916693d03fcfec99e25e791
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-07-18 20:52:43 +0700

    Initial Commit

---
+++ lecuisine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lecuisine
+//
+//  Created by Muhammad Zhuhry on 18/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lecuisine/lecuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecuisineApp.swift
+//  lecuisine
+//
+//  Created by Muhammad Zhuhry on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct lecuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
