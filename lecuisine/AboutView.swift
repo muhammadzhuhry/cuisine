b22e4858246f8f6b595588a438ec2198af0a47ba
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-07-24 19:03:54 +0700

    split about view to different file

---
+++ lecuisine/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  lecuisine
+//
+//  Created by Muhammad Zhuhry on 24/07/22.
+//
+
+import SwiftUI
+
+struct About: View {
+    var body: some View {
+        Text("This is profile")
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        About()
+    }
+}
