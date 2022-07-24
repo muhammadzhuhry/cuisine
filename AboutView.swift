d52912ff11532b36f5bb8b20756771f7c2d35138
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-07-24 19:03:13 +0700

    adding file

---
+++ AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  lecuisine
+//
+//  Created by Muhammad Zhuhry on 24/07/22.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
