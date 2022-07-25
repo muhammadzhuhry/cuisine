f819b7493148eec18b89b8f8101f5645482eabe2
muhammadzhuhry <muhammadzhuhry@gmail.com>
2022-07-25 10:44:42 +0700

    create dynamic list using RecipeData

--- lecuisine/HomeView.swift
+++ lecuisine/HomeView.swift
@@ -30,66 +30,53 @@ struct Header: View {
 }
 
 struct HomeContent: View {
+    var menuList = RecipeData
     var body: some View {
-        List() {
-            HStack(alignment: .top) {
-                Image("food-1")
-                    .resizable()
-                    .renderingMode(.original)
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(20)
-                
-                VStack(alignment: .leading, spacing: 5.0) {
-                    Text("Crock Pot Roast").font(.headline)
+        List(menuList) { menu in
+            NavigationLink(destination: MenuView(menu: menu.name)) {
+                HStack(alignment: .top) {
+                    Image(menu.image)
+                        .resizable()
+                        .renderingMode(.original)
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(20)
                     
-                    HStack {
-                        Image(systemName: "heart.fill")
-                            .foregroundColor(Color.pink)
-                        Text("4.9")
-                            .font(.subheadline)
-                        Text("(150)")
-                            .font(.subheadline)
-                            .foregroundColor(Color.gray)
+                    VStack(alignment: .leading, spacing: 5.0) {
+                        Text(menu.name).font(.headline)
+                        
+                        HStack {
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(Color.pink)
+                            Text("4.9")
+                                .font(.subheadline)
+                            Text("(150)")
+                                .font(.subheadline)
+                                .foregroundColor(Color.gray)
+                        }
+                        
+                        HStack {
+                            Image(systemName: "alarm").foregroundColor(Color.orange)
+                            Text("\(menu.timers.reduce(0, +)) mins")
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
                     }
-                    
-                    HStack {
-                        Image(systemName: "alarm").foregroundColor(Color.orange)
-                        Text("10:00 mins")
-                    }
-                    .font(.subheadline)
-                    .foregroundColor(Color.gray)
-                }
-            }.listRowSeparator(.hidden)
-            
-            HStack(alignment: .top) {
-                Image("food-2")
-                    .resizable()
-                    .renderingMode(.original)
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(20)
-                
-                VStack(alignment: .leading, spacing: 5.0) {
-                    Text("Roasted Asparagus").font(.headline)
-                    
-                    HStack {
-                        Image(systemName: "heart.fill")
-                            .foregroundColor(Color.pink)
-                        Text("4.9")
-                            .font(.subheadline)
-                        Text("(150)")
-                            .font(.subheadline)
-                            .foregroundColor(Color.gray)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "alarm").foregroundColor(Color.orange)
-                        Text("10:00 mins")
-                    }
-                    .font(.subheadline)
-                    .foregroundColor(Color.gray)
-                }
-            }.listRowSeparator(.hidden)
-        }
-        .listStyle(.plain)
+                }.listRowSeparator(.hidden)
+            }
+        }.listStyle(.plain)
+    }
+}
+
+struct MenuView: View {
+    var menu: String
+    
+    var body: some View {
+        Text("Menu: \(menu)")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
     }
 }
